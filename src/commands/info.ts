1e37877501534fbe6bdba29aad8b5fe110c50b59
Lauren Hamel <lauren.hamel@outlook.com>
2026-08-27 23:16:07 -0400

    Renames commands

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,125 +1,45 @@
-import { prepareDownloadResults } from "../core/downloads";
-import { Metadata } from "../core/metadata";
-import { Spoti } from "../core/spoti";
-import { generateTrackTag } from "../core/tags";
+import { getSpotifyType } from "../core/spotify";
 import { type SpotiCliOptions } from "../types/config";
-import { type SpotifyMetadataResult } from "../types/spotify";
 import { createActionHandler } from "../utils/action";
-import { Duration } from "../utils/duration";
-import { Library } from "../utils/library";
-import { Progress } from "../utils/progress";
+import { parseSpotifyURL, validateSpotifyURL } from "../utils/spotify";
 import chalk from "chalk";
-import Table from "cli-table3";
 import { Command } from "commander";
-import { sortBy } from "lodash-es";
-import { type Tags } from "node-id3";
 
-export type InfoCliArgs = [string?];
+export type InfoCliArgs = [string];
 
-export interface InfoCliOptions extends SpotiCliOptions {
-  update: boolean;
-}
+export interface InfoCliOptions extends SpotiCliOptions {}
 
 export default new Command()
   .name("info")
-  .description("Read ID3 tag information from MP3 file(s)")
-  .argument("[file]", "An MP3 file or Spoti metadata file")
-  .option("-u, --update", "Update the ID3 tags of the MP3 file(s)", false)
+  .description("Retrieve information about a Spotify URL")
+  .argument("<url>", "A Spotify URL to retrieve information for")
   .action(
-    createActionHandler<InfoCliArgs, InfoCliOptions>(async (file, options) => {
-      const files: { title: string; file: string; id?: string; tags?: Tags }[] =
-        [];
+    createActionHandler<InfoCliArgs, InfoCliOptions>(async (url, options) => {
+      validateSpotifyURL(url);
 
-      if (file && Library.exists(file)) {
-        const title = Library.title(file);
-        files.push({ file, title });
-      } else if (file && Metadata.has(file)) {
-        const { type, id } = Metadata.read<SpotifyMetadataResult>(file);
-        const items = await Spoti.search(id, type, options);
-        const prepared = prepareDownloadResults(options)(items);
-        const data: {
-          file: string;
-          path: string;
-          title: string;
-          id?: string;
-          tags?: Tags;
-        }[] = [];
+      const { type, id } = parseSpotifyURL(url);
 
-        for (const item of prepared) {
-          const { file, path, title } = item.download;
-          const id = item.track.id;
-          const tags = await generateTrackTag(item);
-          data.push({ file, path, title, id, tags });
-        }
-
-        const sorted = sortBy(data, "title");
-
-        files.push(...sorted);
-      } else {
-        const sorted = Library.files.sort();
-        const mapped = sorted.map((file) => ({
-          file,
-          title: Library.title(file),
-        }));
-        files.push(...mapped);
-      }
-
-      const data = files.map(({ title, file, id, tags }) => ({
-        title,
-        file,
-        id,
-        tags,
-        meta: Library.get(file),
-      }));
-
-      const progress$ = new Progress(
-        "Scanning…",
-        {
-          type: "percentage",
-          percentage: 0,
-          message: `0 / ${files.length}`,
-          nameTransformFn: chalk.blue,
-        },
-        (() => {
-          let reports = 0;
-          return () => {
-            reports++;
-            const percentage = reports / files.length;
-            const message = `${reports} / ${files.length}`;
-            progress$.update(percentage, message);
-          };
-        })()
+      console.log(
+        `Retrieving information for ${chalk.magenta(type)} (${chalk.blue(id)})…`
       );
 
-      const head = ["Title", "Format", "ID", "Size", "Duration"];
-      const table$ = new Table({ head });
-
-      const missing: string[] = [];
-
-      for (const item of data) {
-        const label = Progress.label(item.title, 100);
-
-        if (item.meta) {
-          const { format, size, metadata } = item.meta;
-          const { duration, id = item.id, tags } = await metadata();
-          table$.push([label, format, id, size, Duration.format(duration)]);
-
-          if (options.update) {
-            const patch = { ...tags, ...item.tags };
-            await Library.tag(item.file, patch, id, duration);
-          }
-        } else {
-          missing.push(item.file);
-          table$.push([chalk.dim(label), "-", "-", "-", "-"]);
-        }
-
-        progress$.report();
+      if (options.verbose) {
+        console.log();
+        console.log(chalk.bold.dim("Data"));
+        console.log({ type, id });
+        console.log();
       }
 
-      progress$.done();
-      progress$.remove();
+      const data = await getSpotifyType(id, type, options);
 
-      console.log("");
-      console.log(table$.toString());
+      console.log(data);
+
+      switch (type) {
+        default: {
+          throw new Error(
+            `Sorry, retrieving information for ${type}s not yet supported.`
+          );
+        }
+      }
     })
   );
---
+++ src/commands/tag.ts
@@ -0,0 +1,125 @@
+import { prepareDownloadResults } from "../core/downloads";
+import { Metadata } from "../core/metadata";
+import { Spoti } from "../core/spoti";
+import { generateTrackTag } from "../core/tags";
+import { type SpotiCliOptions } from "../types/config";
+import { type SpotifyMetadataResult } from "../types/spotify";
+import { createActionHandler } from "../utils/action";
+import { Duration } from "../utils/duration";
+import { Library } from "../utils/library";
+import { Progress } from "../utils/progress";
+import chalk from "chalk";
+import Table from "cli-table3";
+import { Command } from "commander";
+import { sortBy } from "lodash-es";
+import { type Tags } from "node-id3";
+
+export type TagCliArgs = [string?];
+
+export interface TagCliOptions extends SpotiCliOptions {
+  update: boolean;
+}
+
+export default new Command()
+  .name("tag")
+  .description("Read ID3 tag(s) from MP3 file(s)")
+  .argument("[file]", "An MP3 file or Spoti metadata file")
+  .option("-u, --update", "Update the ID3 tag(s) of the MP3 file(s)", false)
+  .action(
+    createActionHandler<TagCliArgs, TagCliOptions>(async (file, options) => {
+      const files: { title: string; file: string; id?: string; tags?: Tags }[] =
+        [];
+
+      if (file && Library.exists(file)) {
+        const title = Library.title(file);
+        files.push({ file, title });
+      } else if (file && Metadata.has(file)) {
+        const { type, id } = Metadata.read<SpotifyMetadataResult>(file);
+        const items = await Spoti.search(id, type, options);
+        const prepared = prepareDownloadResults(options)(items);
+        const data: {
+          file: string;
+          path: string;
+          title: string;
+          id?: string;
+          tags?: Tags;
+        }[] = [];
+
+        for (const item of prepared) {
+          const { file, path, title } = item.download;
+          const id = item.track.id;
+          const tags = await generateTrackTag(item);
+          data.push({ file, path, title, id, tags });
+        }
+
+        const sorted = sortBy(data, "title");
+
+        files.push(...sorted);
+      } else {
+        const sorted = Library.files.sort();
+        const mapped = sorted.map((file) => ({
+          file,
+          title: Library.title(file),
+        }));
+        files.push(...mapped);
+      }
+
+      const data = files.map(({ title, file, id, tags }) => ({
+        title,
+        file,
+        id,
+        tags,
+        meta: Library.get(file),
+      }));
+
+      const progress$ = new Progress(
+        "Scanning…",
+        {
+          type: "percentage",
+          percentage: 0,
+          message: `0 / ${files.length}`,
+          nameTransformFn: chalk.blue,
+        },
+        (() => {
+          let reports = 0;
+          return () => {
+            reports++;
+            const percentage = reports / files.length;
+            const message = `${reports} / ${files.length}`;
+            progress$.update(percentage, message);
+          };
+        })()
+      );
+
+      const head = ["Title", "Format", "ID", "Size", "Duration"];
+      const table$ = new Table({ head });
+
+      const missing: string[] = [];
+
+      for (const item of data) {
+        const label = Progress.label(item.title, 100);
+
+        if (item.meta) {
+          const { format, size, metadata } = item.meta;
+          const { duration, id = item.id, tags } = await metadata();
+          table$.push([label, format, id, size, Duration.format(duration)]);
+
+          if (options.update) {
+            const patch = { ...tags, ...item.tags };
+            await Library.tag(item.file, patch, id, duration);
+          }
+        } else {
+          missing.push(item.file);
+          table$.push([chalk.dim(label), "-", "-", "-", "-"]);
+        }
+
+        progress$.report();
+      }
+
+      progress$.done();
+      progress$.remove();
+
+      console.log("");
+      console.log(table$.toString());
+    })
+  );
